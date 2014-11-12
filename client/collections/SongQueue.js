f8efbd28eaa0daaa0658e1d1d3ce8be99ab2f624
tonofprince <brettleibo@gmail.com>
2014-11-12 15:13:43 -0800

    Passed specs for SongQueue, LibraryEntryView, LibraryView, and SongModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,30 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function() {
+      this.remove(this.at(0));
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    }, this);
+
+  },
+
+  playFirst: function() {
+      this.at(0).play();
   }
 
 });
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,8 +7,10 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
-      this.model.play();
-    }
+      this.model.enqueue();
+    },
+
+
   },
 
   render: function(){
